d0e15b361f92303b81f533b2d2d9f63fd330287c
Julia Kummer <julia.kummer98@gmail.com>
2020-11-02 19:05:10 +0100

    finilized Taschenrechner

--- XUnitTestProject1/XUnitTestProject1/TaschenrechnerFeatureSteps.cs
+++ XUnitTestProject1/XUnitTestProject1/TaschenrechnerFeatureSteps.cs
@@ -7,24 +7,35 @@ namespace XUnitTestProject1
     [Binding]
     public class TaschenrechnerFeatureSteps
     {
-        double p; 
+        double p;
         [Given(@"a sinus operator")]
         public void GivenASinusOperator()
         {
-            ScenarioContext.Current.Pending();
+            Math.Cos(p);
         }
-        
+
         [When(@"the object PI")]
         public void WhenTheObjectPI()
         {
             p = Math.PI;
         }
-        
+
         [Then(@"the result should be (.*)")]
         public void ThenTheResultShouldBe(int p0)
         {
-
             Assert.Equal(1.0, p0, 5);
+        }
+
+        [Given(@"a Cos Function")]
+        public void GivenACosFunction()
+        {
+            Math.Cos(p);
+        }
+        
+        [Given(@"a tangens fucntion")]
+        public void GivenATangensFucntion()
+        {
+            Math.Cos(p);
         }
     }
 }
