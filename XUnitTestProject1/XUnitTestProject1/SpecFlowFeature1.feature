bc319c6d560b515dde888278b6656d62c3ff54de
Julia Kummer <julia.kummer98@gmail.com>
2020-11-02 18:59:25 +0100

    Initial commit

---
+++ XUnitTestProject1/XUnitTestProject1/SpecFlowFeature1.feature
@@ -0,0 +1,22 @@
+﻿Feature: Taschenrechner Feature
+	In order to avoid math mistakes
+	As a math idiot
+	I want to calculate the trigonmetric functions
+
+@mytag
+Scenario: Sinus
+	Given a sinus operator
+	When the object PI
+	Then the result should be 0
+
+
+Scenario: Cosinus
+	Given a Cos Function
+	When the object PI
+	Then the result should be -1
+
+Scenario: Tangens
+	Given a tangens fucntion
+	When the object PI
+	Then the result should be 0
+
---
+++ XUnitTestProject1/XUnitTestProject1/TaschenrechnerFeatureSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    [Binding]
+    public class TaschenrechnerFeatureSteps
+    {
+        double p; 
+        [Given(@"a sinus operator")]
+        public void GivenASinusOperator()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the object PI")]
+        public void WhenTheObjectPI()
+        {
+            p = Math.PI;
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+
+            Assert.Equal(1.0, p0, 5);
+        }
+    }
+}
